3f37d6b8e750c46d1516ccf267cb11439edda8b0
sara-stojkov <sarastojkov6@gmail.com>
2026-07-02 21:12:39 +0200

    Feat: integrate network stack into app

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import aws_cdk as cdk
 from hacker_news_bronze_stack.stack import DataCollectionStack
+from network_stack.stack import NetworkStack
 
 app = cdk.App()
 
+network_stack = NetworkStack(app, "NetworkStack")
 DataCollectionStack(
     app,
     "DataCollectionStack",
